cbfd98474470d448a442b9d78ca727efd6dbeabd
Moises <mucalan@live.com>
2024-10-14 22:33:18 -0300

    Classe-4

--- anatomia-das-classes-1/anatomia-das-classes-1/src/MinhaClasse.java
+++ anatomia-das-classes-1/anatomia-das-classes-1/src/MinhaClasse.java
@@ -1,9 +1,17 @@
 public class MinhaClasse {
 
 public  static void main (String [] args) {
+    String primeiroNome = "Moises";
+    String segundoNome = "Bracca";
     
-    System.out.print ("Olá turma, sejam bem vindos");
+    String nomeCompleto = nomeCompleto(primeiroNome, segundoNome);
+    System.out.println(nomeCompleto);
 
-}                  
+
+} 
+
+public static String nomeCompleto (String primeiroNome, String segundoNome){
+    return "Resultado do método "  +  primeiroNome.concat(" ") .concat(segundoNome);
+}
     
 }
